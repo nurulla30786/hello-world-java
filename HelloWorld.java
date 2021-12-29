0eacaa73a8689dda562848c07c3a75dd1c7c38ff
nurulla30786 <91172463+nurulla30786@users.noreply.github.com>
2021-12-29 03:10:31 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     System.out.println("Hello world!");
     System.out.println("Hello Nur");
     System.out.println("Hello Naema");
-System.out.println("Hello Amrulla");
+System.out.println("deleted");
   }
 }
